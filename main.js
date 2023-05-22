535f56db26822e711129125b546787bab9841469
Raymundo Mateos <rayma9829@gmail.com>
2023-05-22 15:36:02 -0600

    [ADD] estructura del html, estilos basicos y funcionalidad basica

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+let messagebox = document.getElementById('messagebox');
+let resultbox = document.getElementById('result');
+let encodeButton = document.getElementById('encode');
+let decodeButton = document.getElementById('decode');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+encodeButton.addEventListener('click', (e) => {
+    const str = messagebox.value;
+    resultbox.value = str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+});
 
-setupCounter(document.querySelector('#counter'))
+
+function encode(text){
+
+}
+
+function decode(hash){
+    
+}
