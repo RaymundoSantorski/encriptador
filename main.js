d309732f8575a7d784cadb1229ee60ebeb2fb217
Raymundo Mateos <rayma9829@gmail.com>
2023-05-24 02:48:58 -0600

    [ADD] funcionalidad de los botones para encriptar, desencriptar y mostrar el resultado y también de copiar

---
+++ main.js
@@ -0,0 +1,45 @@
+const textEncode = document.getElementById('text_encode');
+const textResult = document.getElementById('text_result');
+const buttonDecode = document.getElementsByClassName('button_decode')[0];
+const buttonEncode = document.getElementsByClassName('button_encode')[0];
+const buttonCopy = document.getElementsByClassName('button_copy')[0];
+
+// replace(/[^0-9a-zA-Z. ]/g, '')
+
+textEncode.addEventListener('keyup', () => {
+    const aux = textEncode.value;
+    const text = aux.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[^0-9a-zA-Z ]/g, '').toLowerCase();
+    if(aux !== text) textEncode.value = text;
+});
+
+buttonEncode.addEventListener('click', () => {
+    showResult(encode(textEncode.value));
+});
+
+buttonDecode.addEventListener('click', () => {
+    showResult(decode(textEncode.value));
+});
+
+buttonCopy.addEventListener('click', () => {
+    navigator.clipboard.writeText(textResult.value);
+})
+
+function showResult(result){
+    textResult.value = result;
+}
+
+function encode(text = ''){
+    return text.replaceAll('e', 'enter')
+        .replaceAll('i', 'imes')
+        .replaceAll('a', 'ai')
+        .replaceAll('o', 'ober')
+        .replaceAll('u', 'ufat');
+}
+
+function decode(hash){
+    return hash.replaceAll('ufat', 'u')
+        .replaceAll('ober', 'o')
+        .replaceAll('ai', 'a')
+        .replaceAll('imes', 'i')
+        .replaceAll('enter', 'e');
+}
